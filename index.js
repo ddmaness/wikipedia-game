15e4849e9ea96703e9f5e9f7a34dc81e7e3480e1
Donald Maness <ddmaness@gmail.com>
2018-01-14 14:09:33 -0500

    Make xhttp requests async

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ function startingLinks() {
     player.startingLink = randomQuery();
     player.targetLink = randomQuery();
     player.previousLink = player.startingLink;
+    document.getElementById('starting-link-formated').innerText = removeUrlEncoding(player.startingLink);
+    document.getElementById('target-link-formated').innerText = removeUrlEncoding(player.targetLink);
 }
 
 
@@ -49,13 +51,14 @@ function switchScreen(idToHide, idToReveal) {
 
 // retrieve the summary section of two random elements from the queries array
 function loadSummary(query, queryTwo) {
-    var url = 'https://en.wikipedia.org/w/api.php?action=parse&page=' + query + '&format=json&section=0&prop=text%7Ccategories%7Clinks%7Ctemplates%7Csections%7Crevid%7Cdisplaytitle%7Ciwlinks%7Cproperties%7Cparsewarnings&wrapoutputclass=wiki-output&origin=*'
-    var urlTwo = 'https://en.wikipedia.org/w/api.php?action=parse&page=' + queryTwo + '&format=json&section=0&prop=text%7Ccategories%7Clinks%7Ctemplates%7Csections%7Crevid%7Cdisplaytitle%7Ciwlinks%7Cproperties%7Cparsewarnings&wrapoutputclass=wiki-output&origin=*'
+    openLoadAnimation();
+    var url = 'https://en.wikipedia.org/w/api.php?action=parse&page=' + query + '&format=json&section=0&prop=text%7Ccategories%7Clinks%7Ctemplates%7Csections%7Crevid%7Cdisplaytitle%7Ciwlinks%7Cproperties%7Cparsewarnings&wrapoutputclass=wiki-summary-output&origin=*'
+    var urlTwo = 'https://en.wikipedia.org/w/api.php?action=parse&page=' + queryTwo + '&format=json&section=0&prop=text%7Ccategories%7Clinks%7Ctemplates%7Csections%7Crevid%7Cdisplaytitle%7Ciwlinks%7Cproperties%7Cparsewarnings&wrapoutputclass=wiki-summary-output&origin=*'
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var res = JSON.parse(this.responseText);
-            document.getElementById('starting-link').innerHTML = res.parse.text['*'];
+            document.getElementById('starting-link').lastChild.innerHTML = res.parse.text['*'];
             var links = document.getElementsByTagName('a');
             var linksArr = Array.prototype.slice.call(links);
             linksArr.forEach(function(elem){
@@ -64,8 +67,9 @@ function loadSummary(query, queryTwo) {
             var xhrTwo = new XMLHttpRequest();
             xhrTwo.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
+                    closeLoadAnimation();
                     var res = JSON.parse(this.responseText);
-                    document.getElementById('target-link').innerHTML = res.parse.text['*'];
+                    document.getElementById('target-link').lastChild.innerHTML = res.parse.text['*'];
                     var links = document.getElementsByTagName('a');
                     var linksArr = Array.prototype.slice.call(links);
                     linksArr.forEach(function(elem){
@@ -73,11 +77,11 @@ function loadSummary(query, queryTwo) {
                     })
                 }
             }
-            xhrTwo.open("GET", urlTwo, false);
+            xhrTwo.open("GET", urlTwo, true);
             xhrTwo.send();
         }
     }
-    xhr.open("GET", url, false);
+    xhr.open("GET", url, true);
     xhr.send();
 }
 
@@ -85,6 +89,7 @@ function loadSummary(query, queryTwo) {
 
 function loadJSON(query) {
     // Remove 'start digging button if hasn't been removed already
+    openLoadAnimation();
     var startDiggingButton =document.getElementById('start-digging-button')
     if (!startDiggingButton.classList.contains('removed')){
         startDiggingButton.classList.add('removed');
@@ -95,14 +100,14 @@ function loadJSON(query) {
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             player.article = JSON.parse(this.responseText);
-            setTimeout(function(){
-                document.getElementById('prompt-input').classList.add('is-visible');
-                document.getElementById('prompt-confirm-button').classList.add('is-visible');
-                document.getElementById('prompt-cancel-button').classList.add('is-visible');
-            },1000)
+            displayArticle();
+            closeLoadAnimation();
+            document.getElementById('prompt-input').classList.add('is-visible');
+            document.getElementById('prompt-confirm-button').classList.add('is-visible');
+            document.getElementById('prompt-cancel-button').classList.add('is-visible');
         }
     }
-    xhr.open("GET", url, false);
+    xhr.open("GET", url, true);
     xhr.send();
 }
 
@@ -110,10 +115,16 @@ function loadJSON(query) {
 
 function displayArticle() {
     document.getElementById('page').innerHTML = player.article.parse.text['*'];
+    if(!document.getElementsByClassName('game-links')[0].classList.contains('is-visible')){
+        document.getElementsByClassName('game-links')[0].classList.add('is-visible');
+    }
+    if(!document.getElementsByClassName('game-fixed-nav')[0].classList.contains('is-visible')){
+        document.getElementsByClassName('game-fixed-nav')[0].classList.add('is-visible');
+    }
     var links = document.getElementsByTagName('a');
     var linksArr = Array.prototype.slice.call(links);
     linksArr.forEach(function(elem){
-        if (/^https?:|.ogv$|.webm$|.ogg$|.svg$|Portal:|Special:/.test(elem.href)) {
+        if (/^https?:|.ogv$|.webm$|.ogg$|.svg$|Portal:|Special:|\.php/.test(elem.href)) {
             elem.classList.add('invalid-link');
         }
         else {
@@ -124,31 +135,37 @@ function displayArticle() {
 
 
 
-function confirmConnection() {
-    var userInput = document.getElementById('prompt-input');
-    var previousLink = removeUrlEncoding(player.previousLink);
-    var workingLink = removeUrlEncoding(player.workingLink);
-    if(userInput.value.length === 0) {
-        document.getElementById('prompt-input-required').classList.add('is-visible');
-        return;
-    }
-    if(player.workingLink.toUpperCase() !== player.targetLink.toUpperCase() && player.score >= 9) {
-        endGame('lose')        
-    }
-    if(player.workingLink.toUpperCase() === player.targetLink.toUpperCase()) {
-        endGame('win')
-    }
-    else {
-        document.getElementById('prompt-overlay').classList.remove('is-visible');
-        player.score++;
-        document.getElementById('score').innerText = player.score;
-        player.urlLinks.push(player.previousLink);
-        player.links.push('(' + previousLink + ') is connected to (' + workingLink + ') because ' + userInput.value)
-        player.previousLink = player.workingLink;
-        displayLinks('game-links-list');
-        userInput.value = '';
-        displayArticle();
-    }
+function confirmConnection(event, bool) {
+    if (event.keyCode === 13 || bool === true){
+        event.preventDefault();
+        var query = player.workingLink;
+        var userInput = document.getElementById('prompt-input');
+        if(userInput.value.length === 0) {
+            document.getElementById('prompt-input-required').classList.add('is-visible');
+            return;
+        }
+        if(player.workingLink.toUpperCase() !== player.targetLink.toUpperCase() && player.score >= 9) {
+            endGame('lose')        
+        }
+        if(player.workingLink.toUpperCase() === player.targetLink.toUpperCase()) {
+            endGame('win')
+        }
+        else {
+            if (document.getElementById('game-fixed-nav-previous-btn').classList.contains('is-disabled')){
+                document.getElementById('game-fixed-nav-previous-btn').classList.remove('is-disabled');
+            }
+            document.getElementById('prompt-overlay').classList.remove('is-visible');
+            player.score++;
+            document.getElementById('score').innerText = 10 - player.score;
+            player.urlLinks.push(player.previousLink);
+            player.links.push(inlineElem([removeUrlEncoding(player.previousLink), 'span', 'highlight-secondary'], ['is connected to', 'span'], [removeUrlEncoding(player.workingLink), 'span', 'highlight-secondary'], ['because','span'], [userInput.value, 'span']));
+            player.previousLink = player.workingLink;
+            displayLinks('game-links-list');
+            userInput.value = '';
+            loadJSON(query);
+            scrollToId('game-links-list');
+        }
+    } 
 }
 
 
@@ -162,6 +179,17 @@ function cancelConnection() {
 
 function promptConnection(e){
     if(/#/.test(this.href)){
+        if(!/^\/wiki\/#/.test(this.href)){
+            document.getElementById('prompt-input').classList.remove('is-visible');
+            document.getElementById('prompt-confirm-button').classList.remove('is-visible');
+            document.getElementById('prompt-cancel-button').classList.remove('is-visible');
+            e.preventDefault();
+            var query=/\/wiki\/(.*)#/.exec(this.href)[1];
+            player.workingLink = query;
+            document.getElementById('prompt-text').innerHTML = '';
+            document.getElementById('prompt-text').appendChild(inlineElem([removeUrlEncoding(player.previousLink), 'span', 'highlight-secondary'], ['is connected to', 'span'], [removeUrlEncoding(player.workingLink), 'span', 'highlight-secondary'], ['because...', 'span']))
+            document.getElementById('prompt-overlay').classList.add('is-visible');
+        }
         return;
     }
     document.getElementById('prompt-input').classList.remove('is-visible');
@@ -170,11 +198,9 @@ function promptConnection(e){
     e.preventDefault();
     var query=/\/wiki\/(.*)/.exec(this.href)[1];
     player.workingLink = query;
-    var previousLink = removeUrlEncoding(player.previousLink);
-    var workingLink = removeUrlEncoding(player.workingLink);
-    document.getElementById('prompt-text').innerText = '('+ previousLink + ') is connected to (' + workingLink + ') because...'
+    document.getElementById('prompt-text').innerHTML = '';
+    document.getElementById('prompt-text').appendChild(inlineElem([removeUrlEncoding(player.previousLink), 'span', 'highlight-secondary'], ['is connected to', 'span'], [removeUrlEncoding(player.workingLink), 'span', 'highlight-secondary'], ['because...', 'span']))
     document.getElementById('prompt-overlay').classList.add('is-visible');
-    setTimeout(loadJSON, 100, query);
 }
 
 
@@ -182,6 +208,9 @@ function promptConnection(e){
 function previousArticle() {
     if (player.links.length === 0) {
         return;
+    }
+    if (player.links.length === 1) {
+        document.getElementById('game-fixed-nav-previous-btn').classList.add('is-disabled');
     }
     player.workingLink = player.urlLinks[player.urlLinks.length - 1];
     if (player.urlLinks.length > 1){
@@ -192,6 +221,7 @@ function previousArticle() {
     displayLinks('game-links-list');
     loadJSON(player.workingLink);
     displayArticle();
+    scrollToId('page');
 }
 
 
@@ -200,26 +230,67 @@ function displayLinks(id) {
     while (linkslist.firstChild) {
         linkslist.removeChild(linkslist.firstChild);
     }
-    player.links.forEach(function(elem){
-        var listItem = document.createElement('li')
-        listItem.innerText = elem;
+    player.links.forEach(function(elem, index){
+        var listItem = document.createElement('li');
+        var listArrow = document.createElement('li');
+        var img = document.createElement('img');
+        listArrow.className = 'game-links-list-arrow';
+        img.src='icons/arrow.svg';
+        listArrow.appendChild(img);
+        listItem.appendChild(elem);
+        if (index > 0){
+            linkslist.appendChild(listArrow);
+        }
         linkslist.appendChild(listItem);
     })
+}
+
+function revealInfo(id) {
+    document.getElementById(id).classList.add('is-visible');
+}
+
+
+function hideInfo() {
+    var startingLink = document.getElementById('starting-link');
+    var targetLink = document.getElementById('target-link');
+    if(startingLink.classList.contains('is-visible')) {
+        startingLink.classList.remove('is-visible');
+    }
+    if(targetLink.classList.contains('is-visible')) {
+        targetLink.classList.remove('is-visible');
+    }
 }
 
 
 function endGame(result) {
     var userInput = document.getElementById('prompt-input');
-    var previousLink = removeUrlEncoding(player.previousLink);
-    var workingLink = removeUrlEncoding(player.workingLink);
     player.score++;
-    player.links.push('(' + previousLink + ') is connected to (' + workingLink + ') because ' + userInput.value)
+    player.links.push(highlightStr(player.previousLink) + ' is connected to ' + highlightStr(player.workingLink) + ' because ' + userInput.value)
     userInput.value = '';
     document.getElementById('target-link').innerHTML = '';
     document.getElementById('starting-link').innerHTML = '';    
     document.getElementById('prompt-overlay').classList.remove('is-visible');
     document.getElementById('page').innerHTML = ''
-    switchScreen('game', 'results')
+    switchScreen('game', 'results');
+}
+
+// Takes any number of arguments in this format, an array of strings consisting [string, tag, class, href] where class and href are optional
+function inlineElem() {
+    var args = Array.prototype.slice.call(arguments)
+    var p = document.createElement('p');
+    args.forEach(function(elem){
+        var str = elem[0] + ' ';
+        var tag = document.createElement(elem[1]);
+        tag.innerText = str;
+        if (elem[2]) {
+            tag.className = elem[2];
+        }
+        if (elem[3]) {
+            tag.href = elem[3];
+        }
+        p.appendChild(tag);
+    })
+    return p;
 }
 
 function tryAgain(bool) {
@@ -239,6 +310,11 @@ function tryAgain(bool) {
     switchScreen('results','game');
 }
 
+function scrollToId(id) {
+    document.getElementById(id).scrollIntoView(true);
+    window.scrollBy(0, -60);
+}
+
 function removeInputRequired() {
     document.getElementById('prompt-input-required').classList.remove('is-visible');
 }
@@ -247,4 +323,19 @@ function removeInputRequired() {
 
 function removeUrlEncoding(str) {
     return str.replace(/_/g,' ');
+}
+
+function openLoadAnimation(id){
+    document.getElementById('loading').style.display = 'block';
+    //var spinner = document.createElement('div');
+    //spinner.id = 'spinner';
+    //spinner.innerText = 'LOADING';
+    //spinner.style.animationName = 'spinner';
+    //spinner.style.animationDuration = '2s';
+    //spinner.style.animationIterationCount = 'infinite';
+    //document.getElementById(id).appendChild(spinner);
+}
+
+function closeLoadAnimation() {
+    document.getElementById('loading').style.display = 'none';
 }
